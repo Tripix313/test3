f1e9bb6b65b1b861b711b9ff04d4da3b3f14da0a
Tripix313 <53383825+Tripix313@users.noreply.github.com>
2019-07-28 02:48:32 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get(604796895998509057ا")
+setInterval(function() {
+channel.send(`give mu ccridits hhhhhh`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
